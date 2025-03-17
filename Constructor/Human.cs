ecd8cf7e7ab83092453565da317872481a56a6f8
damirof <feriddmrv1@gmil.com>
2025-03-17 18:10:33 +0400

    my first commit

---
+++ Constructor/Human.cs
@@ -0,0 +1,43 @@
+﻿namespace Constructor
+{
+    internal class Human
+    {
+
+        public Human()
+        {
+        }
+        public Human(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public Human(string name)
+        {
+
+            this.name = name;
+        }
+        public Human(string name, string surname, byte age) : this(name, surname)
+        {
+            age = age;
+        }
+
+        public string name;
+        public string surname;
+        public byte age;
+
+
+        public void ShowFullData()
+        {
+            Console.WriteLine($"Name:{name}, Surname{surname}, Age:{age}");
+        }
+
+        public string GetFullData()
+        {
+            string fullname = this.name + "" + this.surname;
+            return fullname;
+        }
+    }
+
+
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+/*
+ 
+ 
+ Human classiniz olur
+Constructorunun 3 overloadi olur (bos; name; name, surname) qebul edir
+Name
+Surname
+Age
+fieldleri var.
+ShowFullData()
+GetFullName() methodlari var
+
+
+Human class:
+============
+string Name
+string Surname;
+byte Age (encapsulation)
+
+
+User class:
+===========
+- UserName - istifadəçinin istifadəçi adını ifadə edir
+- Password - İstifadəçinin şifrə dəyərini ifadə edir
+
+
+
+
+User classi Human'dan miras alir. Name, Surname, UserName dəyəri təyin olunmamış User obyekti yaradıla bilinməməlidir.UserName dəyərinin uzunluğu minimum 6, maksimum 25 ola bilər.
+Password dəyərinin uzunluğu minimum 8 , maksimum 25 ola bilər.
+
+
+Daha sonra User type-da olan obyekt yaradın, bunun üçün username və password dəyərlərini console-dan qəbul edin.*/
+
+
+
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human("Nezrin", "Eliyeva ");
+
+
+            human.age = 10;
+            human.ShowFullData();
+        }
+
+
+    }
+}
